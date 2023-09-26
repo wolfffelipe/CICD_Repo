97fe46f00f0597de276620a157ca2463bfc630b5
felipe.wolff <wolff.felipe@hotmail.com>
2023-09-26 16:54:35 -0300

    Nova coluna

--- CICD_DB/dbo/Tables/Usuario.sql
+++ CICD_DB/dbo/Tables/Usuario.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Usuario] (
     [ID_Usuario] INT           IDENTITY (1, 1) NOT NULL,
     [NM_Usuario] VARCHAR (100) NULL,
-    [cpf]        VARCHAR (11)  NULL
+    [NR_Cpf]        VARCHAR (11)  NULL, 
+    [DT_Nasc] DATE NULL
 );
 
